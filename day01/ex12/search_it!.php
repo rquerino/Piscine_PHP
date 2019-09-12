ddbade95b79b434ba5105df2d714032c03548d8f
Renato Querino <renatoqrn@gmail.com>
2019-09-11 18:10:19 -0700

    ex12 ok

--- day01/ex12/search_it!.php
+++ day01/ex12/search_it!.php
@@ -1,3 +1,19 @@
 <?php
-
+	array_shift($argv);
+	if ($argc > 2)
+	{
+		$key = $argv[0];
+		$ar = array();
+		array_shift($argv);
+		$res = NULL;
+		$i = 0;
+		foreach ($argv as $av)
+		{
+			$ar = explode(":", $av);
+			if ($key == $ar[0])
+				$res = $ar[1];
+		}
+		if ($res != NULL)
+			echo "$res\n";
+	}
 ?>
