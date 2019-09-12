de1b9eb1297c8a4fcf90259d926435e5a7e0ddd5
Renato Querino <renatoqrn@gmail.com>
2019-09-12 16:53:51 -0700

    ex03 ok

---
+++ day02/ex03/who.php
@@ -0,0 +1,14 @@
+<?php
+	$who = shell_exec("w");
+	$ar = preg_split('/\n/', trim($who)); // trim line and divide by '\n'
+	//ignore two lines
+	array_shift($ar);
+	array_shift($ar);
+	foreach ($ar as $av)
+	{
+		$obj = preg_split('/\s+/', $av);
+		$date = date('M d', time());
+		$tty = ($obj[1] == "console") ? $obj[1] : "tty".$obj[1];
+		echo $obj[0] . ' ' . str_pad($tty, 8) . ' ' . $date . ' ' . $obj[3] . "\n";
+	}
+?>
