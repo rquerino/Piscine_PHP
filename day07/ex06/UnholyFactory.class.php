b192ae59855b0e5030f57484f5a03755bb00ac09
Renato Querino <renatoqrn@gmail.com>
2019-09-19 14:19:45 -0700

    ex06 ok

--- day07/ex06/UnholyFactory.class.php
+++ day07/ex06/UnholyFactory.class.php
@@ -1,57 +1,48 @@
 <?php
-    class UnholyFactory {
-        private $mas = array();
-        private $manufactured = array();
-        private $absorb;
-        private $type_formatted;
+	class UnholyFactory {
+		private $absorbed = array();
+		private $type_formatted;
 
-        public function absorb($type)
-        {
-            if ($type instanceof Fighter)
-            {
-                if (in_array($type, $this->mas)) // If it's already inside
-                    printf("(Factory already absorbed a fighter of type ");
-                else
-                {
-                    printf("(Factory absorbed a fighter of type ");
-                    $this->mas[] = $type;
-                }
-                printf($type->getType() . ")\n");
-            }
-            else
-                printf("(Factory can't absorb this, it's not a fighter)\n");
-        }
+		public function absorb($soldier)
+		{
+			if ($soldier instanceof Fighter)
+			{
+				if (in_array($soldier, $this->absorbed)) // If it's already inside
+					printf("(Factory already absorbed a fighter of type %s)\n", $soldier->getType());
+				else
+				{
+					$this->absorbed[] = $soldier;
+					printf("(Factory absorbed a fighter of type %s)\n", $soldier->getType());
+				}
+			}
+			else
+				printf("(Factory can't absorb this, it's not a fighter)\n");
+		}
 
-        private function format($tmp)
-        {
-            if ($tmp === "foot soldier")
-                return "Footsoldier";
-            else if ($tmp === "llama")
-                return "Llama";
-            else if ($tmp === "archer")
-                return "Archer";
-            else if ($tmp === "assassin")
-                return "Assassin";
-        }
+		private function formatType($type_soldier)
+		{
+			if ($type_soldier === "llama")
+				return "Llama";
+			else if ($type_soldier === "archer")
+				return "Archer";
+			else if ($type_soldier === "foot soldier")
+				return "Footsoldier";
+			else if ($type_soldier === "assassin")
+				return "Assassin";
+		}
 
-        public function fabricate($tmp)
-        {
-            $this->type_formatted = $this->format($tmp);
-            foreach ($this->mas as $key => $value) {
-                if (get_class($value) === $this->type_formatted)
-                {
-                    $new = clone $this->mas[$key];
-                    printf("(Factory fabricates a fighter of type " . $tmp . ")". PHP_EOL);
-                    return ($new);
-                }
-            }
-            printf("(Factory hasn't absorbed any fighter of type ". $tmp . ")". PHP_EOL);
-        }
-        
-        public function fight($j)
-        {
-            echo "OK";
-            print_r($this->manufactured);
-        }
-    }
+		public function fabricate($type)
+		{
+			$this->type_formatted = $this->formatType($type);
+			foreach ($this->absorbed as $key => $value) {
+				if (get_class($value) === $this->type_formatted)
+				{
+					$fabricated = clone $this->absorbed[$key];
+					printf("(Factory fabricates a fighter of type %s)" . PHP_EOL, $type);
+					return ($fabricated);
+				}
+			}
+			printf("(Factory hasn't absorbed any fighter of type %s)" . PHP_EOL, $type);
+		}
+	}
 ?>
